41c965035ca0dd1298464660bcb19c0a5299e4d4
Imran khan <imranloveh01@gmail.com>
2024-04-24 02:29:42 +0500

    initialize the project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Link href={'https://youtu.be/Big_aFLmekI?t=851'}>start video</Link>
+  );
+}
