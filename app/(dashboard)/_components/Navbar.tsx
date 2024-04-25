2247db11e1bcc7e52daa48846d6c96be5e80f0e6
Imran khan <imranloveh01@gmail.com>
2024-04-25 18:26:41 +0500

    Update Navbar and SidebarRoutes components

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,3 +1,5 @@
+import NavbarRoutes from "@/components/NavbarRoutes"
+import MobileSidebar from "./MobileSidebar"
 
 
 
@@ -6,7 +8,8 @@ const Navbar = () => {
     <div 
     className="p-4 border-b h-full flex items-center bg-white shadow-sm"
     >
-d
+        <MobileSidebar />
+        <NavbarRoutes />
     </div>
   )
 }
--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,37 +1,52 @@
-'use client'
+"use client";
 
-import { Compass, Layout } from "lucide-react"
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import SidebarItem from "./SidebarItem";
+import { usePathname } from "next/navigation";
 const guestRoutes = [
     {
-        icon:Layout,
-        label:'Dashboard',
-        href:'/'
+        icon: Layout,
+        label: "Dashboard",
+        href: "/",
     },
     {
-        icon:Compass,
-        label:'Browse',
-        href:'/search'
-    }
-]
+        icon: Compass,
+        label: "Browse",
+        href: "/search",
+    },
+];
+
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses",
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics",
+    },
+];
 
 const SidebarRoutes = () => {
-    const routes = guestRoutes;
-  return (
-    <div className="flex flex-col w-full">
-    {
-        routes.map((route)=>(
-            <SidebarItem
-            key={route.href}
-            icon={route.icon}
-            label={route.label}
-            href={route.href}
-            
-            />
-        ))
-    }
-    </div>
-  )
-}
+    const pathname = usePathname();
+    const isTeacherRoutes = pathname.includes("/teacher");
 
-export default SidebarRoutes
+    const routes = isTeacherRoutes ? teacherRoutes : guestRoutes;
+
+    return (
+        <div className="flex flex-col w-full">
+            {routes.map((route) => (
+                <SidebarItem
+                    key={route.href}
+                    icon={route.icon}
+                    label={route.label}
+                    href={route.href}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default SidebarRoutes;
