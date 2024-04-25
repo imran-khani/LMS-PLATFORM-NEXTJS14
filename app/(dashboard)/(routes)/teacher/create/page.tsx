c5e06ede138f8a7ec0dd7129a46ad05d0de131bc
Imran khan <imranloveh01@gmail.com>
2024-04-25 21:23:04 +0500

    Update CreateCourse component with form validation and submission logic

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
-import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import axios from 'axios'
-import {useForm} from 'react-hook-form'
-import { useRouter } from 'next/navigation'
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import {
     Form,
@@ -12,28 +12,49 @@ import {
     FormDescription,
     FormField,
     FormLabel,
-    FormMessage
-} from '@/components/ui/form'
+    FormMessage,
+} from "@/components/ui/form";
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-    title:z.string().min(1,{
-        message:'Title is required'
-    })
-})
-
+    title: z.string().min(1, {
+        message: "Title is required",
+    }),
+});
 
 const CreateCourse = () => {
-
+    
     const form = useForm<z.infer<typeof formSchema>>({
-        resolver:zodResolver(formSchema)
-    })
-  return (
-    <div>CreateCourse</div>
-  )
-}
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        },
+    });
 
-export default CreateCourse
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
+    return (
+        <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+            <div>
+                <h1 className="text-2xl">Name your course</h1>
+                <p className="text-sm text-slate-600 ">
+                    What would you like to name your course? Don't worry, you
+                    can change this later.
+                </p>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8"
+                    ></form>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default CreateCourse;
