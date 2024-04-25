6d5669ba9a40f2fead53453b8371ff32f5b75ead
Imran khan <imranloveh01@gmail.com>
2024-04-25 20:37:15 +0500

    Add form validation and submission logic to CreateCourse component

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,6 +1,36 @@
+'use client'
+
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import {useForm} from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+
+const formSchema = z.object({
+    title:z.string().min(1,{
+        message:'Title is required'
+    })
+})
 
 
 const CreateCourse = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver:(formSchema)
+    })
   return (
     <div>CreateCourse</div>
   )
