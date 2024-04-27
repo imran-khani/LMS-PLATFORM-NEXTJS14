fcf3a3e24dddd6e01182022fd061bba9e38a3134
Imran khan <imranloveh01@gmail.com>
2024-04-27 23:17:43 +0500

    Refactor fileUpload component to use UploadDropzone from lib/uploadthing

--- components/fileUpload.tsx
+++ components/fileUpload.tsx
@@ -1,18 +1,30 @@
-import { UploadDropzone } from "@uploadthing/react";
+"use client";
 
-import { OurFileRouter, ourFileRouter } from "@/app/api/uploadthing/core";
+import toast from "react-hot-toast";
 
+import { UploadDropzone } from "@/lib/uploadthing";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 interface FileUploadProps {
-    onChange:(url:string)=> void;
-    endpoint:keyof typeof ourFileRouter
-}
+  onChange: (url?: string, originalFilename?: string) => void;
+  endpoint: keyof typeof ourFileRouter;
+};
 
+export const FileUpload = ({
+  onChange,
+  endpoint
+}: FileUploadProps) => {
 
-const fileUpload = () => {
   return (
-    <div>fileUpload</div>
-  )
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        console.log("onClientUploadComplete res:", res);
+        onChange(res?.[0].url, res?.[0].name);
+      }}
+      onUploadError={(error: Error) => {
+        toast.error(`${error?.message}`);
+      }}
+    />
+  );
 }
-
-export default fileUpload
