e645a1b5264d8c7dd80eed59acd4e6734222e537
Imran khan <imranloveh01@gmail.com>
2024-05-09 00:19:29 +0500

    refactor: Update ChapterId component to include user authentication

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,6 +1,7 @@
+import { IconBadge } from "@/components/iconBadge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -36,7 +37,7 @@ const ChapterId = async ({ params }: { params: { courseId: string; chapterId: st
   // Count the number of completed fields
   const completedFields = requiredFields.filter(field => field).length;
 
-  const completetionText = `(${totalFields}/${completedFields})`;
+  const completetionText = `(${completedFields}/${totalFields})`;
 
   return (
     <div className="p-6">
@@ -49,6 +50,31 @@ const ChapterId = async ({ params }: { params: { courseId: string; chapterId: st
           <ArrowLeft className="h-4 w-4 mr-2" />
           Back to course
           </Link>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">
+                    Chapter Creation
+                </h1>
+                <span
+                className="text-sm text-slate-700"
+                >
+                    Complete all fields {completetionText}
+
+                </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="space-y-4">
+            <div>
+                <div className="flex items-center gap-x-2">
+                    <IconBadge icon={LayoutDashboard} />
+                    <h2 className="text-xl">
+                        Customize your chapter
+                    </h2>
+                </div>
+            </div>
         </div>
       </div>
     </div>
