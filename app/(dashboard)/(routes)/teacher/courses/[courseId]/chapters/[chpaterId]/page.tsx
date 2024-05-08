85134297d85db2374d1f295a9663fbdcd07cc385
Imran khan <imranloveh01@gmail.com>
2024-05-08 22:40:49 +0500

    feat: Add user authentication to ChapterId component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chpaterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chpaterId]/page.tsx
@@ -1,9 +1,12 @@
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 
 const ChapterId = async ({params}:{params:{courseId:string; chapterId:string}}) => {
 
   const {userId} = auth()
+  if (!userId) redirect('/')
+    
   return (
     <div>
 
