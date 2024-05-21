cd81bdce7d49acd3fbf615a6cfc134e14f2d3f99
Imran khan <imranloveh01@gmail.com>
2024-05-21 22:06:45 +0500

    refactor: Add lazy loading for images in CategoryItem component

--- app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
+++ app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
@@ -1,8 +1,12 @@
 'use client'
 
 
+import { cn } from '@/lib/utils';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
 import { IconType } from 'react-icons';
+
+import qs from 'query-string';
 
 interface CategoryItemProps {
     label: string;
@@ -13,12 +17,45 @@ interface CategoryItemProps {
 
 const CategoryItem = ({
     label,
-    icon,
+    icon: Icon,
     value
 }: CategoryItemProps) => {
+    const router = useRouter()
+    const pathname = usePathname()
+    const searhchParams = useSearchParams()
+
+    const currentCategoryId = searhchParams.get('categoryId')
+    const currentTitle = searhchParams.get('title')
+    const isSelected = currentCategoryId === value;
+
+
+    const setQuery = () => {
+        const url = qs.stringifyUrl({
+            url: pathname,
+            query: {
+                title: currentTitle,
+                categoryId: isSelected ? null : value
+            }
+        }, { skipNull: true, skipEmptyString: true });
+
+        router.push(url)
+    }
 
     return (
-        <div>CategoryItem</div>
+        <button
+            className={
+                cn(
+                    'flex items-center gap-x-1 px-3 border text-sm py-2 rounded-full transition border-slate-200 hover:border-sky-700'
+                    ,
+                    isSelected ? 'border-sky-700' : ''
+                )
+            }
+            type='button'
+            onClick={setQuery}
+        >
+            {Icon && <Icon size={20} />}
+            <div className='truncate'>{label}</div>
+        </button>
     )
 }
 
---
+++ components/search-input.tsx
@@ -0,0 +1,12 @@
+'use clinet'
+
+
+import React from 'react'
+
+const SearchInput = () => {
+  return (
+    <div>SearchInput</div>
+  )
+}
+
+export default SearchInput
