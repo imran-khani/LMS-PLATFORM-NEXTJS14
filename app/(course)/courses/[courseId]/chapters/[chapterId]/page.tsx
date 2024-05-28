32b02bea55be0f3f0c51b9626be1bdaadaf35333
Imran khan <imranloveh01@gmail.com>
2024-05-28 16:24:48 +0500

    feat: Add lazy loading for images in ChapterId component

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,22 +1,38 @@
+import { getChaptes } from "@/actions/get-chapters";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 interface IParams {
 
-    params:{
-        courseId:string;
-        chapterId:string;
-    }
+    courseId: string;
+    chapterId: string
 }
-const ChapterId = (params:IParams) => {
+const ChapterId = async (params: IParams) => {
 
-    const {userId} = auth()
-    if(!userId) return redirect('/');
+    const { userId } = auth()
+    if (!userId) return redirect('/');
 
-    
-  return (
-    <div>ChapterId</div>
-  )
+    const {
+        attachments,
+        chapter,
+        course,
+        muxData,
+        nextChapter,
+        purchase,
+        userProgress
+    } = await getChaptes({
+        courseId: params.courseId,
+        userId,
+        chapterId: params.chapterId
+    })
+
+    if (!course || !chapter) { return redirect('/') };
+    const isLocked = !chapter.isFree && !purchase;
+
+
+    return (
+        <div>ChapterId</div>
+    )
 }
 
 export default ChapterId
