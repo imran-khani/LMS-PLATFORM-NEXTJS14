6bd37c2beca1a11075373aeafddd67ad3d19d004
Imran khan <imranloveh01@gmail.com>
2024-06-07 16:48:14 +0500

    video player updated with onEnd function

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/VideoPlayer.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/VideoPlayer.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+"use client";
 
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -8,6 +7,7 @@ import MuxPlayer from "@mux/mux-player-react";
 import { useState } from "react";
 import { Loader2, Lock } from "lucide-react";
 import { cn } from "@/lib/utils";
+import useConfetti from "@/hooks/use-confetti-store";
 
 interface VideoProps {
     chapterId: string;
@@ -19,7 +19,6 @@ interface VideoProps {
     completeOnEnd: boolean;
 }
 
-
 const VideoPlayer = ({
     chapterId,
     courseId,
@@ -27,47 +26,59 @@ const VideoPlayer = ({
     nextChapterId,
     playbackId,
     completeOnEnd,
-    title
+    title,
 }: VideoProps) => {
+    const [isReady, setIsReady] = useState(false);
+    const router = useRouter();
+    const confetti = useConfetti();
 
-    const [isReady, setIsReady] = useState(false)
+    const onEnd = async () => {
+        try {
+            if (completeOnEnd) {
+                await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
+                    isCompleted: true,
+                });
+
+                if (!nextChapterId) {
+                    confetti.onOpen();
+                }
+
+                toast.success("Progress updated");
+                router.refresh();
+
+                if (nextChapterId) {
+                    router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+                }
+            }
+        } catch {
+            toast.error("Something went wrong");
+        }
+    }
     return (
-        <div
-            className="relative aspect-video"
-        >
-            {
-                !isReady && !isLocked && (
-                    <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
-                        <Loader2
-                            className="h-8 w-8 animate-spin text-secondary"
-                        />
-                    </div>
-                )}
-            {
-                isLocked && (
-                    <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
-                        <Lock className="h-8 w-8" />
-                        <p className="text-sm">
-                            This chapter is Locked
-                        </p>
-                    </div>
-                )}
-            {
-                !isLocked && (
-                    <MuxPlayer
-                        title={title}
-                        className={cn(
-                            !isReady && 'hidden'
-                        )}
-                    onCanPlay={()=>setIsReady(true)}
-                    onEnded={()=>{}}
+        <div className="relative aspect-video">
+            {!isReady && !isLocked && (
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+                    <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+                </div>
+            )}
+            {isLocked && (
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+                    <Lock className="h-8 w-8" />
+                    <p className="text-sm">This chapter is Locked</p>
+                </div>
+            )}
+            {!isLocked && (
+                <MuxPlayer
+                    title={title}
+                    className={cn(!isReady && "hidden")}
+                    onCanPlay={() => setIsReady(true)}
+                    onEnded={onEnd}
                     autoPlay
                     playbackId={playbackId}
-                    />
-                )
-            }
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
